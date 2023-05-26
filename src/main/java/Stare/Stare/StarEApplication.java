48562d1420dfd14c03e1e61d3f6631139363c1bf
LeeHanByeol <byol0425@gmail.com>
2023-05-27 02:53:05 +0900

    Initial commit

---
+++ src/main/java/Stare/Stare/StarEApplication.java
@@ -0,0 +1,13 @@
+package Stare.Stare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Stare/Stare/StarEApplicationTests.java
@@ -0,0 +1,13 @@
+package Stare.Stare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
